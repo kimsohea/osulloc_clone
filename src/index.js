a806ecb83c33ee716210239b9874ed75bbc0ba60
kimsohea <kimsohea4425@gmail.com>
2025-03-03 19:49:07 +0900

    tool: scss 추가

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import { greet } from "./component.js";
+import "./scss/style.scss";
 
 const script = () => {
-	return `
+  return `
   ${greet("헤더 될 예정입니다")}
   ${greet("main 될 예정입니다")}
   ${greet("footer 될 예정입니다")}
--- webpack.config.js
+++ webpack.config.js
@@ -8,34 +8,39 @@ const __dirname = path.dirname(__filename);
 
 // Webpack 설정을 export
 export default {
-	mode: "development",
-	entry: "./src/index.js", // 시작 파일
-	output: {
-		filename: "bundle.js",
-		path: path.resolve(__dirname, "dist"), // 번들 파일 저장 경로
-		clean: true, // 기존 파일 삭제
-	},
-	devtool: "source-map",
-	devServer: {
-		static: path.resolve(__dirname, "public"), // 정적 파일 경로
-		port: 3000, // 개발 서버 포트
-		open: true, // 자동으로 브라우저 열기
-		hot: false, // HMR 비활성화
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/, // .js 파일을 대상으로
-				exclude: /node_modules/, // node_modules 제외
-				use: {
-					loader: "babel-loader",
-				},
-			},
-		],
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: "./public/index.html",
-		}),
-	],
+  mode: "development",
+  entry: "./src/index.js", // 시작 파일
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"), // 번들 파일 저장 경로
+    clean: true, // 기존 파일 삭제
+  },
+  devtool: "source-map",
+  devServer: {
+    static: path.resolve(__dirname, "public"), // 정적 파일 경로
+    port: 3000, // 개발 서버 포트
+    open: true, // 자동으로 브라우저 열기
+    hot: false, // HMR 비활성화
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // .js 파일을 대상으로
+        exclude: /node_modules/, // node_modules 제외
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        exclude: /node_modules/, // node_modules 제외
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
 };
